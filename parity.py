b344aef23b294bcf260dd72838321f1ba5fb2a31
kaykongo <pkkongo@st.ug.edu.gh>
2025-04-20 20:46:08 +0000

    enhance: made the return logic for is_even function more succinct

--- parity.py
+++ parity.py
@@ -1,14 +1,12 @@
 # Python code to check if a number is even or odd (hence its mathematical name parity)
-
-number = int(input("Guess a number: "))
-
-def check_parity(number=0):
-    if number%2 == 0:
-        print("Your number is even!")
-    else:
-        print("Your number is odd!")
-
 def main():
-    check_parity(number)
+    x = int(input("What\'s x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return n % 2 == 0
 
 main()
