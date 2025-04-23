daf2b7fb98981bad42867b3e973890b9a0bc5397
kaykongo <pkkongo@st.ug.edu.gh>
2025-04-23 00:57:35 +0000

    feat: improved the loop; learnt how to ask users for input while inducing an infinite loop til they give me what I want

--- cat.py
+++ cat.py
@@ -1,8 +1,24 @@
-# n = 0
+# # n = 0
 
-# while n < 3:
+# # while n < 3:
+# #     print("meow")
+# #     n += 1
+
+# # the _ variable is not used by the user/human. It is only requisite to run a specific functionality in python.
+# for _ in range(3):
 #     print("meow")
-#     n += 1
 
-for i in [4, 1, -6]:
-    print("meow")
+# print("meaow\n"* 3, end="")
+
+def main():
+    make_me_meow(x="")
+
+def make_me_meow(x):
+    while True:
+        x = int(input("What\'s x? "))
+        if x > 0:
+            break
+    for _ in range(x):
+        print("Meow")
+
+main()
