c294d5b821a5fc63ee2d0204bfee11165be2b6eb
kaykongo <pkkongo@st.ug.edu.gh>
2025-04-29 13:31:34 +0000

    feat: multiple arguments passed to sys.argv

--- name.py
+++ name.py
@@ -3,8 +3,7 @@ import sys
 # Check for errors
 if len(sys.argv) < 2:
     sys.exit("Yo! You forgot to add your first name in the CLI")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments in the CLI. Gyae gyimie nu!")
 
 # Execute Code
-print("Hello! My name is", sys.argv[1])
+for arg in sys.argv[1:]:
+    print("Hello! My name is", arg)
