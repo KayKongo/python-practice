6c40387f4a6ecf7a986210f6aa5b9930915ef423
kaykongo <pkkongo@st.ug.edu.gh>
2025-04-29 11:06:54 +0000

    feat: played around with the random and statistics modules

---
+++ average.py
@@ -0,0 +1,5 @@
+import statistics
+
+average = statistics.mean([100, 90, 50, 10, 50])
+
+print(average)
--- generate.py
+++ generate.py
@@ -1,12 +1,25 @@
 import random
 
-def get_coin_toss():
-    return random.choice(["heads", "tails"])
+# random.choice()
+# def get_coin_toss():
+#     return random.choice(["heads", "tails"])
 
-def main():
-    if get_coin_toss() == "heads":
-        print('You won!')
-    else:
-        print("You lost")
+# def main():
+#     if get_coin_toss() == "heads":
+#         print('You won!')
+#     else:
+#         print("You lost")
 
-main()
+# main()
+
+
+# random.randint()
+# number = random.randint(1, 10)
+# print(number)
+
+
+# random.shuffle()
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
