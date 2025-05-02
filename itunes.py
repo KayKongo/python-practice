e3be9fb2f2d0a5221c6b94d1c1a8f97ded6b16e7
kaykongo <pkkongo@st.ug.edu.gh>
2025-05-02 06:52:08 +0000

    feat: learnt how to make API requests using the PYPI requests module

--- itunes.py
+++ itunes.py
@@ -1,8 +1,18 @@
+import json
 import requests
 import sys
 
 if len(sys.argv) != 2:
     sys.exit("Kindly specify the song you want to search for")
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(response.json())
+# response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+# pretty_response = json.dumps(response.json(), indent=2)
+# print(pretty_response)
+
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+# print(json.dumps(response.json(), indent=2))
+obj = response.json()
+
+for result in obj["results"]:
+    print(result["trackName"])
