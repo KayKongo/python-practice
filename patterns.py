d92e61240292b9d27ca35b3b631229b58864c46c
kaykongo <pkkongo@st.ug.edu.gh>
2025-06-09 16:13:46 +0000

    feat: Made the patterns interactive where users can input the number of rows and columns they want to print

--- patterns.py
+++ patterns.py
@@ -1,10 +1,9 @@
-pattern = [1,2,3,4]
-length = len(pattern)
+length = int(input("How many rows and coloumns do you want to print? => "))
 
-# for i in range(length):
-#     print("*" * length)
+for i in range(length):
+    print("*" * length)
 
 # code to print the list of even numbers from 0 - 20
-for _ in range(0, 21, 2):
-    print(_)
+# for _ in range(0, 21, 2):
+#     print(_)
 
