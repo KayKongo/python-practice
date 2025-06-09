8e38d6634a61dee96c0b93658c8f02e1373f5fac
kaykongo <pkkongo@st.ug.edu.gh>
2025-06-09 15:55:47 +0000

    feat: print even numbers from 0 through 20

--- patterns.py
+++ patterns.py
@@ -1,5 +1,10 @@
 pattern = [1,2,3,4]
 length = len(pattern)
 
-for i in range(length):
-    print("*" * length)
+# for i in range(length):
+#     print("*" * length)
+
+# code to print the list of even numbers from 0 - 20
+for _ in range(0, 21, 2):
+    print(_)
+
